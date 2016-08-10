5465efe1beb6c1e42e0d0f8460c8ac966bd87866
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-08-10 11:15:56 -0700

    remove /users//listings tests since that no longer exists

--- selbi-backend/tests/listings.test.js
+++ selbi-backend/tests/listings.test.js
@@ -1,42 +1,13 @@
 import FirebaseTest, { minimalUserUid } from './FirebaseTestConnections';
 // import { expect } from 'chai';
 
-describe('listings', () => {
+describe('/listings', () => {
   before(function (done) {
     this.timeout(6000);
-    FirebaseTest.dropDatabase()
+    FirebaseTest
+      .dropDatabase()
       .then(done)
       .catch(done);
   });
 
-  describe('/users/$uid/listing', () => {
-    const usersRef = FirebaseTest.minimalUserApp.database().ref('/users/');
-
-    it('accepts complete listings objects', (done) => {
-      const modifiedMinimalUserData = FirebaseTest.getMinimalUserData();
-      modifiedMinimalUserData.listings = {
-        private: { l1: true },
-        public: { l1: true },
-        inactive: { l2: true },
-        sold: { l3: true },
-        salePending: { l4: true },
-      };
-      usersRef
-        .child(minimalUserUid)
-        .set(modifiedMinimalUserData)
-        .then(done)
-        .catch(done);
-    });
-
-    it('accepts empty listings objects', (done) => {
-      // This test passes because Firebase ignores empty objects.
-      const modifiedMinimalUserData = FirebaseTest.getMinimalUserData();
-      modifiedMinimalUserData.listings = {};
-      usersRef
-        .child(FirebaseTest.minimalUserUid)
-        .set(modifiedMinimalUserData)
-        .then(done)
-        .catch(done);
-    });
-  });
 });
