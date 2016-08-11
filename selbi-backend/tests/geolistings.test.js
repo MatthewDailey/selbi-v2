f34b8514bad0896e13c458449817154c7f0cda21
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-08-11 11:37:16 -0700

    tests covering storing and querying geolistings

--- selbi-backend/tests/geolistings.test.js
+++ selbi-backend/tests/geolistings.test.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import GeoFire from 'geofire';
 import FirebaseTest, { testUserUid } from './FirebaseTestConnections';
 
-function createListingWithId(listindId) {
+function createTestUserListingWithId(listindId) {
   return FirebaseTest
     .testUserApp
     .database()
@@ -11,7 +11,7 @@ function createListingWithId(listindId) {
     .set(FirebaseTest.getTestUserListingOne());
 }
 
-function geoFireRef() {
+function testUserGeoFireRef() {
   const geolistingsRef = FirebaseTest
     .testUserApp
     .database()
@@ -39,10 +39,44 @@ describe('/geolistings', () => {
   });
 
   it('cannot create geolisting without listing', (done) => {
-    geoFireRef()
+    testUserGeoFireRef()
       .set('nonExistantListingId', [37.79, -122.41])
       .then(() => {
-        done(new Error('Should not be able to store a geolisting for non-existant listing.'))
+        done(new Error('Should not be able to store a geolisting for non-existant listing.'));
+      })
+      .catch((error) => {
+        expect(error.code).to.equal('PERMISSION_DENIED');
+        done();
+      });
+  });
+
+  it('cannot create arbitrary data on geolistings', (done) => {
+    FirebaseTest
+      .testUserApp
+      .database()
+      .ref('/geolistings')
+      .child('newChild')
+      .set('somedata')
+      .then(() => {
+        done(new Error('Should not be able to store arbitrary data in geolisting.'));
+      })
+      .catch((error) => {
+        expect(error.code).to.equal('PERMISSION_DENIED');
+        done();
+      });
+  });
+
+  it('cannot store geolisting for other user', (done) => {
+    const listingId = 'minimalUserListing';
+    FirebaseTest
+      .minimalUserApp
+      .database()
+      .ref('/listings')
+      .child(listingId)
+      .set(FirebaseTest.getMinimalUserListingOne())
+      .then(() => testUserGeoFireRef().set('newListingId', [37.79, -122.41]))
+      .then(() => {
+        done(new Error('Should not be able to store geolisting for different users listing.'));
       })
       .catch((error) => {
         expect(error.code).to.equal('PERMISSION_DENIED');
@@ -51,31 +85,61 @@ describe('/geolistings', () => {
   });
 
   it('can create geolisting for listing', (done) => {
-    createListingWithId('newListingId')
-      .then(() => geoFireRef().set('newListingId', [37.79, -122.41]))
+    createTestUserListingWithId('newListingId')
+      .then(() => testUserGeoFireRef().set('newListingId', [37.79, -122.41]))
       .then(done)
       .catch(done);
   });
 
-  // it('create GeoFire index', (done) => {
-  //
-  //
-  //   const logResult = (data) => console.log(data)
-  //
-  //   const promiseKey1 = geoFire.set('key1', ).then(logResult);
-  //   const promiseKey2 = geoFire.set('key2', [37.79, -122.41]).then(logResult);
-  //   const promiseKey3 = geoFire.set('key3', [37.79, 122.41]).then(logResult);
-  //
-  //   Promise.all([promiseKey1, promiseKey2, promiseKey3])
-  //     .then(() => {
-  //       const geoQuery = geoFire.query({
-  //         center: [37.79, -122.41],
-  //         radius: 10.5,
-  //       });
-  //       geoQuery.on('ready', () => console.log('ready'));
-  //       geoQuery.on('key_entered', (data) => console.log(`entered: ${data}`));
-  //       geoQuery.on('key_exited', (data) => console.log(`exited: ${data}`));
-  //       geoQuery.on('key_moved', (data) => console.log(`moved: ${data}`));
-  //     });
-  // });
+  it('can query geolistings', (done) => {
+    const listingId = 'newListingId';
+    createTestUserListingWithId(listingId)
+      .then(() => testUserGeoFireRef().set(listingId, [37.79, -122.41]))
+      .then(() => {
+        const geoQuery = testUserGeoFireRef().query({
+          center: [37.79, -122.41],
+          radius: 10.5,
+        });
+        geoQuery.on('key_entered', (data) => {
+          expect(data).to.equal(listingId);
+        });
+        geoQuery.on('key_exited', () => {
+          done(new Error('Unexpected call to key_exited on geo query.'));
+        });
+        geoQuery.on('key_moved', () => {
+          done(new Error('Unexpected call to key_moved on geo query.'));
+        });
+        geoQuery.on('ready', () => {
+          geoQuery.cancel();
+          done();
+        });
+      })
+      .catch(done);
+  });
+
+  it('query only finds proximate listings', (done) => {
+    const listingId = 'newListingId';
+    createTestUserListingWithId(listingId)
+      .then(() => testUserGeoFireRef().set(listingId, [37.79, 122.41]))
+      .then(() => {
+        const geoQuery = testUserGeoFireRef().query({
+          center: [37.79, -122.41],
+          radius: 10.5,
+        });
+        geoQuery.on('key_entered', () => {
+          done(new Error('Unexpected call to key_entered on geo query.'));
+        });
+        geoQuery.on('key_exited', () => {
+          done(new Error('Unexpected call to key_exited on geo query.'));
+        });
+        geoQuery.on('key_moved', () => {
+          done(new Error('Unexpected call to key_moved on geo query.'));
+        });
+        geoQuery.on('ready', () => {
+          geoQuery.cancel();
+          done();
+        });
+      })
+      .catch(done);
+  });
 });
