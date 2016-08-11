35662614b293eba3ab4e31ffd9b85f6371ed3f98
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-08-11 08:42:28 -0700

    add tests covering security of userListings writes

--- selbi-backend/tests/userListings.test.js
+++ selbi-backend/tests/userListings.test.js
@@ -43,6 +43,7 @@ describe('/userListings', () => {
       .child(testUserUid)
       .set(FirebaseTest.getUserListingPartial());
 
+    // Use service account to store userLIsting because normal users can't modify sold+salePending.
     const promiseStoreUserListingSold = FirebaseTest
       .serviceAccountApp
       .database()
@@ -67,4 +68,68 @@ describe('/userListings', () => {
       })
       .catch(done);
   });
+
+  it('user cannot write entire userListing with sold or salePending', (done) => {
+    FirebaseTest
+      .minimalUserApp
+      .database()
+      .ref('/userListings')
+      .child(minimalUserUid)
+      .set(FirebaseTest.getUserListingSoldAndSalePending())
+      .then(() => {
+        done(new Error('Should not be able to write to salePending or sold'));
+      })
+      .catch((error) => {
+        expect(error.code).to.equal('PERMISSION_DENIED');
+        done();
+      });
+  });
+
+  function testUserCannotWriteTo(userListingType) {
+    it(`user cannot write to /userListings/$uid/${userListingType}`, (done) => {
+      FirebaseTest
+        .minimalUserApp
+        .database()
+        .ref('/userListings')
+        .child(minimalUserUid)
+        .child(userListingType)
+        .update({
+          listingOne: true,
+        })
+        .then(() => {
+          done(new Error(`Should not be able to update /userListings/$uid/${userListingType}`));
+        })
+        .catch((error) => {
+          expect(error.code).to.equal('PERMISSION_DENIED');
+          done();
+        });
+    });
+  }
+
+  function testUserCanWriteTo(userListingType) {
+    it(`user can write to /userListings/$uid/${userListingType}`, (done) => {
+      FirebaseTest
+        .minimalUserApp
+        .database()
+        .ref('/userListings')
+        .child(minimalUserUid)
+        .child(userListingType)
+        .update({
+          listingOne: true,
+        })
+        .then(done)
+        .catch(done);
+    });
+  }
+
+  testUserCanWriteTo('inactive');
+
+  testUserCanWriteTo('private');
+
+  testUserCanWriteTo('public');
+
+  testUserCannotWriteTo('sold');
+
+  testUserCannotWriteTo('salePending');
+
 });
