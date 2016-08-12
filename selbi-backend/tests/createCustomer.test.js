05c6128dec1a99051afa54e425c85512e9ab8e35
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-08-12 14:57:49 -0700

    Add createCustomer queue

---
+++ selbi-backend/tests/createCustomer.test.js
@@ -0,0 +1,50 @@
+import { expect } from 'chai';
+import Queue from 'firebase-queue';
+import FirebaseTest from './FirebaseTestConnections';
+
+function testSafeWorker(worker, done) {
+  return (data, progress, resolve, reject) => {
+    try {
+      worker(data);
+      resolve();
+      done();
+    } catch (e) {
+      reject(e);
+      done(e);
+    }
+  };
+}
+
+describe('Create Customer', () => {
+  beforeEach((done) => {
+    FirebaseTest
+      .serviceAccountApp
+      .database()
+      .ref('/createCustomer')
+      .remove()
+      .then(done);
+  });
+
+  it('can create queue and handle work', function (done) {
+    this.timeout(10000);
+
+    const testData = {
+      foo: 'bar',
+    };
+
+    const queue = new Queue(
+      FirebaseTest.serviceAccountApp.database().ref('/createCustomer'),
+      testSafeWorker(
+        (data) => {
+          expect(data.foo).to.equal(testData.foo);
+        },
+        done));
+
+    FirebaseTest
+      .serviceAccountApp
+      .database()
+      .ref('/createCustomer/tasks')
+      .child('testData')
+      .set(testData);
+  });
+});
