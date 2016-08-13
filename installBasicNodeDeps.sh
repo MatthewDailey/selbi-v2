71ce10dad5041c5fbc4f326edb84a02f730b1611
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-08-13 09:41:43 -0700

    update node project init script to include ESLint

--- installBasicNodeDeps.sh
+++
@@ -1,4 +0,0 @@
-npm install babel-core --save
-npm install babel-preset-es2015 --save
-npm install chai --save
-
---
+++ startNodeProject.sh
@@ -0,0 +1,22 @@
+npm init
+
+npm install babel-core --save
+npm install babel-preset-es2015 --save
+npm install chai --save
+
+npm install eslint --save-dev
+npm install eslint-config-airbnb --save-dev
+npm install eslint-plugin-import --save-dev
+npm install eslint-plugin-jsx-a11y --save-dev
+npm install eslint-plugin-react --save-dev
+
+echo "Creating .eslintrc for project root..."
+echo "{\n\t\"extends\": \"airbnb\",\n\t\"env\": {\n\t\t\"node\": true\n\t}\n}" > .eslintrc
+
+mkdir -p tests
+
+echo "Creating tests dir and tests/.eslintrc..."
+echo "{\n\t\"env\": {\n\t\t\"mocha\": true\n\t}\n}" > tests/.eslintrc
+
+echo "Done installing node deps and eslint. To enable ESLint in Intellij go to 'Languages & Frameworks > JavaScript > Code Quality Tools > ESLint' and check enabled."
+
