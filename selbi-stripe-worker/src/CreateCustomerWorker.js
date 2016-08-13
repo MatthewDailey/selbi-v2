f67cbb9189313fd6d2ecc2a192e6a7df773bc4a2
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-08-13 10:51:57 -0700

    introduce git flow and staging vs produciton branch

--- selbi-stripe-worker/src/CreateCustomerWorker.js
+++ selbi-stripe-worker/src/CreateCustomerWorker.js
@@ -1,6 +1,17 @@
-
+import Queue from 'firebase-queue';
+import firebase from 'firebase';
 
 class CreateCustomerWorker {
+
+  constructor() {
+    const serviceAccountConfig = {
+      serviceAccount: '../firebase-service-accounts/service-account.json',
+      databaseURL: 'https://selbi-staging.firebaseio.com',
+    };
+
+    this.firebaseApp = firebase.initializeApp(serviceAccountConfig);
+  }
+
   start() {
 
   }
