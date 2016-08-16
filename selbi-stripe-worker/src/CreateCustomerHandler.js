64784100aa518a75445fa0d8b653ef06ba963f2d
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-08-16 16:17:08 -0700

    add spy test verifying calling stripe.customers.create

--- selbi-stripe-worker/src/CreateCustomerHandler.js
+++ selbi-stripe-worker/src/CreateCustomerHandler.js
@@ -17,6 +17,30 @@ function validateData(data, reject) {
 }
 
 /*
+ * This method demonstrates how to create a Customer which can be used for repeated payments. It
+ * is intended to be run server side.
+ *
+ * @param payload.source String token returned to the client device from Stripe when a payment
+ * source was created.
+ * @param payload.description String describing the customer.
+ * @param payload.email
+ *
+ * @returns Promise fulfilled with client data.
+ */
+function createCustomer(stripeCustomerApi, payload) {
+  return new Promise((resolve, reject) => {
+    stripeCustomerApi.create(payload, (err, customer) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(customer);
+      }
+    });
+  });
+}
+
+
+/*
  * This class provides the Firebase-Queue listener which is used to create a Stripe Customer
  * based on the enqueued user data.
  *
@@ -24,13 +48,20 @@ function validateData(data, reject) {
  * data in Firebase at /customers/$uid
  */
 class CreateCustomerHandler {
-  constructor(firebaseDb, stripeCustomersApi) {
+  constructor(firebaseDb, stripeCustomerApi) {
     this.firebaseDb = firebaseDb;
-    this.stripeCustomersApi = stripeCustomersApi;
+    this.stripeCustomerApi = stripeCustomerApi;
   }
 
   handleTask(data, progress, resolve, reject) {
     validateData(data, reject);
+
+    return createCustomer(this.stripeCustomerApi, data.payload)
+      .then((customerData) => {
+        console.log(customerData)
+        // update firebase db.
+      })
+      .catch((error) => reject(error));
   }
 }
 
--- selbi-stripe-worker/tests/CreateCustomerHandler.test.js
+++ selbi-stripe-worker/tests/CreateCustomerHandler.test.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import sinon from 'sinon';
+import { spy } from 'sinon';
 import CreateCustomerHandler from '../src/CreateCustomerHandler';
 
 const testCreateCustomerTask = {
@@ -15,16 +15,43 @@ function deepCopy(obj) {
   return JSON.parse(JSON.stringify(obj));
 }
 
+/*
+ * Mock the Stripe Customer API which allows us to avoid actually calling the Stripe server
+ * during unit tests.
+ *
+ * @param error Error arg to be passed to create callback when stripe.customers.create called.
+ * @param customer Customer data to be passed to create callback when stripe.customers.create
+ * called.
+ *
+ * @returns {Object} result.api is the mock api to pass in, result.createSpy is a spy watching the
+ * create method on the API object.
+ */
+function getSpyForStripeCustomersApi(error, customer) {
+  const stripeCustomersApi = {
+    create(payload, callback) {
+      callback(error, customer);
+    },
+  };
+  return {
+    api: stripeCustomersApi,
+    createSpy: spy(stripeCustomersApi, 'create'),
+  };
+}
+
+
 describe('CreateCustomerHandler', () => {
+  /* eslint-disable no-unused-expressions */
+  /* This is necessary for the chai expect statements */
+
   describe('data validation', () => {
     beforeEach(function () {
-      this.progress = sinon.spy();
-      this.resolve = sinon.spy();
-      this.reject = sinon.spy();
-      this.firebaseRoot = sinon.spy();
+      this.progress = spy();
+      this.resolve = spy();
+      this.reject = spy();
+      this.firebaseRoot = spy();
     });
 
-    it('must have uid', function() {
+    it('must have uid', function () {
       const dataMinus = deepCopy(testCreateCustomerTask);
       delete dataMinus.uid;
 
@@ -77,6 +104,31 @@ describe('CreateCustomerHandler', () => {
 
       expect(this.reject.called).to.be.true;
       expect(this.resolve.called).to.be.false;
+    });
+  });
+
+  describe('stripe calls', () => {
+    beforeEach(function () {
+      this.progress = spy();
+      this.resolve = spy();
+      this.reject = spy();
+      this.firebaseDb = {};
+    });
+
+    it('calls stripe.customers.create', function (done) {
+      const stripeCustomersApiMock = getSpyForStripeCustomersApi(null, { customer: 'data' });
+
+      new CreateCustomerHandler(this.firebaseDb, stripeCustomersApiMock.api)
+        .handleTask(
+          deepCopy(testCreateCustomerTask),
+          this.progress,
+          this.resolve,
+          this.reject)
+        .then(() => {
+          expect(stripeCustomersApiMock.createSpy.called).to.be.true;
+        })
+        .then(done)
+        .catch(done);
     });
   });
 });
