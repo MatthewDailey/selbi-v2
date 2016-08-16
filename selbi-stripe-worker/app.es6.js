dfc2e96228bafe8d697641eabf48eb4713b51a77
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-08-16 15:20:59 -0700

    sketch out high level CreateCustomer driver

--- selbi-stripe-worker/app.es6.js
+++ selbi-stripe-worker/app.es6.js
@@ -1,22 +1,31 @@
 import express from 'express';
 import firebase from 'firebase';
+import initializeStripe from 'stripe';
+
 import ServiceAccount from '@selbi/service-accounts';
+import CreateCustomerHandler from './src/CreateCustomerHandler';
+import CreateCustomerQueueListener from './src/CreateCustomerQueueListener';
+
+const stripe = initializeStripe(process.env.STRIPE_PRIVATE);
 
 const serviceAccountApp = firebase.initializeApp(ServiceAccount.firebaseConfigFromEnvironment(),
   'serviceUser');
+const firebaseDb = serviceAccountApp.database();
 
-function createListing() {
-  serviceAccountApp
-    .database()
-    .ref('test')
-    .push({ c: 1 })
-    .then(() => {
-      console.log('created test element.');
-    })
-    .catch(console.log);
-}
+const createCustomerHandler = new CreateCustomerHandler(firebaseDb, stripe.customers);
+const createCustomerQueueListener = new CreateCustomerQueueListener();
 
-createListing();
+createCustomerQueueListener.start(firebaseDb, createCustomerHandler.handleTask);
+
+process.on('SIGINT', () => {
+  console.log('Received SIGINT, starting graceful shutdown...');
+
+  createCustomerQueueListener
+    .shutdown()
+    .then(() => serviceAccountApp.delete())
+    .then(() => console.log('Graceful shutdown of firebase connections complete.'))
+    .then(() => process.exit(0));
+});
 
 const app = express();
 
