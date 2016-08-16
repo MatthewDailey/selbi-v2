a06e591221a39b86866ac9ab3c01ef223bc1ff54
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-08-16 12:15:31 -0700

    [stripe worker] test covering that CreateCustomerWorker calls handler on enqueue

--- selbi-stripe-worker/tests/CreateCustomerWorker.test.js
+++ selbi-stripe-worker/tests/CreateCustomerWorker.test.js
@@ -1,8 +1,53 @@
 import { expect } from 'chai';
-import sinon, { once } from 'sinon';
+import sinon from 'sinon';
+import firebase from 'firebase';
+import ServiceAccount from '@selbi/service-accounts';
 import CreateCustomerWorker from '../src/CreateCustomerWorker';
 
+const testUserUid = 'CreateCustomerWorker-TestUser';
+
+const testCreateCustomerData = {
+  payload: {
+    source: 'stripePaymentCcToken',
+    description: 'test user',
+    email: 'matt@selbi.io',
+  },
+  uid: testUserUid,
+};
+
 describe('CreateCustomerWorker', () => {
+  let firebaseApp = null;
+
+  before(function (done) {
+    this.timeout(5000);
+
+    firebaseApp = firebase.initializeApp(
+      ServiceAccount.firebaseConfigFromEnvironment(),
+      'test-driver');
+
+    firebaseApp
+      .database()
+      .ref('/')
+      .remove()
+      .then(() => firebaseApp
+        .database()
+        .ref('/users')
+        .child(testUserUid)
+        .set({
+          message: 'This is a strawman. The CreateCustomerWorker test does not care about user ' +
+          'data integrity.',
+        }))
+      .then(done)
+      .catch(done);
+  });
+
+  after((done) => {
+    firebaseApp
+      .delete()
+      .then(done)
+      .catch(done);
+  });
+
   it('can be created twice as long as shutdown', (done) => {
     new CreateCustomerWorker().start(() => {}).shutdown()
       .then(() => new CreateCustomerWorker().start(() => {}).shutdown())
@@ -10,13 +55,42 @@ describe('CreateCustomerWorker', () => {
       .catch(done);
   });
 
-  describe('queue listening', () => {
-    it('sinon test', () => {
-      const callback = sinon.spy();
+  describe('queue listening', function () {
+    this.timeout(5000);
 
-      callback();
+    beforeEach(function () {
+      this.worker = new CreateCustomerWorker();
+    });
 
-      expect(callback.called).to.be.true;
+    afterEach(function (done) {
+      this.worker
+        .shutdown()
+        .then(() => done())
+        .catch(done);
+    });
+
+    it('does not call queueHandler off the bat', function () {
+      const queueHandler = sinon.spy();
+
+      this.worker.start(queueHandler);
+
+      expect(queueHandler.called).to.be.false;
+    });
+
+    it('calls handler when data is queued', function (done) {
+      const queueHandler = sinon.spy((data, progress, resolve, reject) => {
+        expect(data.uid).to.equal(testUserUid);
+        resolve()
+          .then(done);
+      });
+
+      this.worker.start(queueHandler);
+
+      firebaseApp
+        .database()
+        .ref('/createCustomer/tasks')
+        .push(testCreateCustomerData)
+        .catch(done);
     });
   });
 });
