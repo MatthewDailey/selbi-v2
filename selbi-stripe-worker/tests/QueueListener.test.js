ebc66837001168760f49ab36035107c190da5a08
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-08-17 00:29:28 -0700

    add tests covering QueueListener for '/createAccount'

--- selbi-stripe-worker/tests/QueueListener.test.js
+++ selbi-stripe-worker/tests/QueueListener.test.js
@@ -15,7 +15,39 @@ const testCreateCustomerData = {
   uid: testUserUid,
 };
 
-describe('CreateCustomerQueueListener', () => {
+const testCreateAccountData = {
+  payload: {
+    external_account: 'stripeConnectBankToken',
+    email: 'user@selbi.io',
+    // managed: true,
+    // country: 'US',
+    legal_entity: {
+      dob: {
+        day: 20,
+        month: 3,
+        year: 1990,
+      },
+      first_name: 'test',
+      last_name: 'user',
+      // type: 'individual', // only other option is 'company'
+      address: {
+        line1: '655 Natoma Street',
+        line2: 'Apt C',
+        city: 'San Francisco',
+        postal_code: '94103',
+        state: 'CA',
+      },
+      personal_id_number: 'stripeSsnToken',
+    },
+    tos_acceptance: {
+      date: Date.now() / 1000,
+      ip: '12.23.34.45',
+    },
+  },
+  uid: testUserUid,
+};
+
+describe('QueueListener', () => {
   before(function (done) {
     this.timeout(5000);
 
@@ -57,7 +89,7 @@ describe('CreateCustomerQueueListener', () => {
       .catch(done);
   });
 
-  describe('queue listening', function () {
+  describe('createCustomer Queue Listening', function () {
     this.timeout(5000);
 
     beforeEach(function () {
@@ -76,7 +108,7 @@ describe('CreateCustomerQueueListener', () => {
 
       this.worker.start(this.firebaseApp.database(), queueHandler);
 
-      expect(queueHandler.called).to.be.false;
+      expect(queueHandler.called).to.equal(false);
     });
 
     it('calls handler when data is queued', function (done) {
@@ -92,6 +124,45 @@ describe('CreateCustomerQueueListener', () => {
         .database()
         .ref('/createCustomer/tasks')
         .push(testCreateCustomerData)
+        .catch(done);
+    });
+  });
+
+  describe('createAccount Queue Listening', function () {
+    this.timeout(5000);
+
+    beforeEach(function () {
+      this.worker = new QueueListener('/createAccount');
+    });
+
+    afterEach(function (done) {
+      this.worker
+        .shutdown()
+        .then(() => done())
+        .catch(done);
+    });
+
+    it('does not call queueHandler off the bat', function () {
+      const queueHandler = sinon.spy();
+
+      this.worker.start(this.firebaseApp.database(), queueHandler);
+
+      expect(queueHandler.called).to.equal(false);
+    });
+
+    it('calls handler when data is queued', function (done) {
+      const queueHandler = sinon.spy((data, progress, resolve, reject) => {
+        expect(data.uid).to.equal(testUserUid);
+        resolve()
+          .then(done);
+      });
+
+      this.worker.start(this.firebaseApp.database(), queueHandler);
+
+      this.firebaseApp
+        .database()
+        .ref('/createAccount/tasks')
+        .push(testCreateAccountData)
         .catch(done);
     });
   });
