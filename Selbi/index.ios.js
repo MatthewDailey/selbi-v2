05543c074adac1e8c2cc4eba021b81dd58737e4a
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-08-19 10:58:02 -0700

    factor out nav and menu to own component

--- Selbi/index.ios.js
+++ Selbi/index.ios.js
@@ -1,93 +1,10 @@
-import React, { Component } from 'react'
-import { AppRegistry, View, ScrollView, ListView, Navigator, Text, TouchableHighlight } from 'react-native'
-import NavigationBar from 'react-native-navbar'
-import Drawer from 'react-native-drawer';
+import React from 'react';
+import { AppRegistry } from 'react-native';
 
-import Camera from './components/Camera'
-import ListingsView from './components/ListingsView'
-import Menu from './components/Menu'
-
-
-const drawerStyles = {
-  drawer: { shadowColor: '#000000', shadowOpacity: 0.8, shadowRadius: 0},
-  main: {paddingLeft: 0},
-};
-
-class RightExpandingNavWithMenuDrawer extends React.Component {
-  // noinspection Eslint - This syntax is necessary to properly bind the method.
-  openMenu = () => {
-    this.drawer.open()
-  };
-
-  render() {
-    const menuButtonConfig = () => { return {
-      title: 'Menu',
-      handler: this.openMenu,
-    }};
-
-    const backButtonConfig = (navigator) => { return {
-      title: '< Back',
-      handler: () => navigator.pop()
-    }};
-
-    const doneButtonConfig = (navigator) => { return {
-      title: 'Done',
-      handler: () => navigator.popToTop()
-    }};
-
-    const nextButtonConfig = (navigator, title, nextRoute) => { return {
-      title: title,
-      handler: () => navigator.push(nextRoute)
-    }};
-
-    const getLeftButton = (navigator, routeIndex) => {
-      if (routeIndex == 0) {
-        return menuButtonConfig(navigator);
-      }
-      return backButtonConfig(navigator);
-    };
-
-    const getRightButton = (navigator, routeIndex) => {
-      if (this.props.routes[routeIndex + 1]) {
-        return nextButtonConfig(navigator,
-          this.props.routes[routeIndex].nextLabel,
-          this.props.routes[routeIndex + 1])
-      }
-      return doneButtonConfig(navigator);
-    };
-
-    return (
-      <Drawer
-        ref={(c) => {
-          this.drawer = c;
-        }}
-        content={this.props.menu}
-        tapToClose
-        styles={drawerStyles}
-        openDrawerOffset={0.2}
-        panOpenMask={0.1}
-      >
-        <Navigator
-          initialRoute={this.props.routes[0]}
-          renderScene={(route, navigator) => {
-            return (
-              // Note this flex:1 style. Super fucking important to make sure listview can scroll.
-              // Without it, the view will just bounce back. Who the fuck knows why.
-              <View style={{flex: 1}}>
-                <NavigationBar
-                  title={{title: route.title}}
-                  leftButton={ getLeftButton(navigator, route.index) }
-                  rightButton={ getRightButton(navigator, route.index) }
-                />
-                {route.renderContent()}
-              </View>
-            );
-          }}
-        />
-      </Drawer>
-    );
-  }
-}
+import Camera from './components/Camera';
+import ListingsView from './components/ListingsView';
+import Menu from './components/Menu';
+import RightExpandingNavWithMenuDrawer from './components/RightExpandingNavWithMenuDrawer';
 
 const localListingRoutes = [
   { title: 'Listings Near You',
@@ -96,11 +13,11 @@ const localListingRoutes = [
     index: 0 },
   { title: 'Create Listing',
     renderContent: () => <Camera />,
-    index: 1 }
+    index: 1 },
 ];
 
 function Application() {
-  return <RightExpandingNavWithMenuDrawer routes={localListingRoutes} menu={<Menu />} />
+  return <RightExpandingNavWithMenuDrawer routes={localListingRoutes} menu={<Menu />} />;
 }
 
 
