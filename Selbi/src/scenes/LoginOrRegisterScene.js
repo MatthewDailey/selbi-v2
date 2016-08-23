ce9ba63dc6a5b24b467cc5c0ee33fb8667a4a2e5
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-08-23 11:33:13 -0700

    Add basic testing functionality for react native components

---
+++ Selbi/src/scenes/LoginOrRegisterScene.js
@@ -0,0 +1,12 @@
+import React, { Component, View } from 'react';
+
+export default class LoginOrRegisterScene extends Component {
+  render() {
+    // TODO
+    return <View />;
+  }
+}
+
+LoginOrRegisterScene.propTypes = {
+  // TODO
+};
