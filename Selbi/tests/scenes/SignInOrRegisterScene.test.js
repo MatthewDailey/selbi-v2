5ce2c5be48697fd6eab89e084e403c034cca1cda
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-08-28 12:09:49 -0700

    test covering initial state of the component

--- Selbi/tests/scenes/SignInOrRegisterScene.test.js
+++ Selbi/tests/scenes/SignInOrRegisterScene.test.js
@@ -5,8 +5,28 @@ import { expect } from 'chai';
 import SignInOrRegisterScene from '../../src/scenes/SignInOrRegisterScene';
 
 describe('<SignInOrRegisterScene />', () => {
+  const stateEmailSignIn = 'emailSignIn';
+  const stateEmailRegister = 'emailRegister';
+  const statePasswordSignIn = 'passwordSignIn';
+  const statePasswordRegister = 'passwordRegister';
+
+
   it('can render', () => {
     const wrapper = shallow(<SignInOrRegisterScene />);
     expect(wrapper.length).to.equal(1);
+  });
+
+  it('has default state', () => {
+    const wrapper = shallow(<SignInOrRegisterScene />);
+    expect(wrapper.state()).to.have.all.keys([
+      stateEmailSignIn,
+      stateEmailRegister,
+      statePasswordSignIn,
+      statePasswordRegister,
+    ]);
+    expect(wrapper.state(stateEmailSignIn)).to.equal('');
+    expect(wrapper.state(stateEmailRegister)).to.equal('');
+    expect(wrapper.state(statePasswordSignIn)).to.equal('');
+    expect(wrapper.state(statePasswordRegister)).to.equal('');
   });
 });
