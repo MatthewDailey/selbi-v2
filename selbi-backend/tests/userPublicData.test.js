18c907acc8da07349fb60cfaf49d04a1eeaa029b
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-09-01 08:16:17 -0700

    Introduce /userPublicData endpoint
    We need to store this info in a public place so that loadin user name doesn't
    also load bank deets.

---
+++ selbi-backend/tests/userPublicData.test.js
@@ -0,0 +1,64 @@
+import { expect } from 'chai';
+import FirebaseTest, { minimalUserUid, expectUnableToStore } from '@selbi/firebase-test-resource';
+
+const sampleUserPublicData = {
+  displayName: 'Matt Dailey',
+  profileImageUrl: 'https://scontent.fsnc1-1.fna.fbcdn.net/v/t1.0-9/12741983_10208490353834533' +
+    '_7138930131848794213_n.jpg?oh=726eae9fde50187d2f14d5012c6c4fe5&oe=5852666A',
+};
+
+describe('/userPublicData', () => {
+  before(function (done) {
+    this.timeout(6000);
+    FirebaseTest
+      .dropDatabase()
+      .then(() => FirebaseTest.createMinimalUser())
+      .then(done)
+      .catch(done);
+  });
+
+  it('user can create own data', (done) => {
+    FirebaseTest
+      .minimalUserApp
+      .database()
+      .ref('userPublicData')
+      .child(minimalUserUid)
+      .set(sampleUserPublicData)
+      .then(done)
+      .catch(done);
+  });
+
+  it('user cannot create data for another user', (done) => {
+    expectUnableToStore(
+      FirebaseTest
+        .testUserApp
+        .database()
+        .ref('userPublicData')
+        .child(minimalUserUid)
+        .set(sampleUserPublicData))
+      .then(done)
+      .catch(done);
+  });
+
+  it('can read another users data', (done) => {
+    FirebaseTest
+      .minimalUserApp
+      .database()
+      .ref('userPublicData')
+      .child(minimalUserUid)
+      .set(sampleUserPublicData)
+      .then(() => FirebaseTest
+        .testUserApp
+        .database()
+        .ref('userPublicData')
+        .child(minimalUserUid)
+        .once('value'))
+      .then((minimalUserPublicData) => {
+        expect(minimalUserPublicData.exists());
+        expect(minimalUserPublicData.val().displayName).to.equal(sampleUserPublicData.displayName);
+      })
+      .then(done)
+      .catch(done);
+  });
+});
+
