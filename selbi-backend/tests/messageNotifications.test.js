8f357a889da72450be37ff58949253cd1058eb41
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-09-14 16:22:11 -0700

    introduce messageNotifications queue in schema and add skeleton for tests

---
+++ selbi-backend/tests/messageNotifications.test.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import Queue from 'firebase-queue';
+import FirebaseTest, { testUserUid } from '@selbi/firebase-test-resource';
+
+describe('/messageNotifications', () => {
+  it('passes', () => {
+    // Blank test.
+  });
+});
+
