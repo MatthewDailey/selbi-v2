fb936224165df1f69a2a9c073265c6387dd1e0d5
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-09-24 22:20:20 -0700

    add shadow to back chevron

--- Selbi/src/scenes/ListingDetailScene.js
+++ Selbi/src/scenes/ListingDetailScene.js
@@ -403,15 +403,30 @@ class ListingDetailScene extends RoutableScene {
           source={{ uri: `data:image/png;base64,${imageData.base64}` }}
           style={styles.container}
         >
-          <View
-            style={{
-              height: 48,
-              width: 48,
-              opacity: 0.7,
-            }}
-          >
-            {this.getLeftButton()}
-          </View>
+            <TouchableHighlight
+              style={{
+                alignItems: 'center',
+                paddingTop: 32,
+                paddingLeft: 8,
+                height: 64,
+                width: 48,
+                opacity: 0.7,
+              }}
+              onPress={this.goBackHandler}
+              underlayColor={colors.transparent}
+              activeOpacity={0.5}
+            >
+              <Text
+                style={{
+                  textShadowColor: colors.dark,
+                  textShadowOffset: {
+                    width: 1,
+                    height: 1,
+                  },
+                  textShadowRadius: 2,
+                }}
+              ><Icon name="chevron-left" size={18} color={colors.secondary} /></Text>
+            </TouchableHighlight>
           <View
             style={{
               flex: 1,
