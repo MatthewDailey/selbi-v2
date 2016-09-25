f108e24d42c1fe42da7020993bd14680139f139d
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-09-24 23:17:59 -0700

    add config.js to repo

---
+++ Selbi/selbiBuildResources/develop/config.js
@@ -0,0 +1,9 @@
+export default {
+  firebase: {
+    apiKey: 'AIzaSyCmaprrhrf42pFO3HAhmukTUby_mL8JXAk',
+    authDomain: 'selbi-develop.firebaseapp.com',
+    databaseURL: 'https://selbi-develop.firebaseio.com',
+    storageBucket: 'selbi-develop.appspot.com',
+  },
+  codePushKey: 'XYU_wAvoNeTzW_UuGogZ5kImnomjVybu8pY2Z',
+};
---
+++ Selbi/selbiBuildResources/staging/config.js
@@ -0,0 +1,9 @@
+export default {
+  firebase: {
+    apiKey: 'AIzaSyA4INDO4i3EOjpBY8d3mUsENeA2WzvX7CE',
+    authDomain: 'selbi-staging.firebaseapp.com',
+    databaseURL: 'https://selbi-staging.firebaseio.com',
+    storageBucket: 'selbi-staging.appspot.com',
+  },
+  codePushKey: 'QddCD3NAHvgwHAH3rcEa45hJGMFgVybu8pY2Z',
+};
