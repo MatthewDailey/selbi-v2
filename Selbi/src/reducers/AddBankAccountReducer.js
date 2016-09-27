293d39b4f5d791cfa5ff692e523bd3f7c046d29b
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-09-27 11:59:29 -0700

    introduce AddBankAcccountReducer as flattened record

---
+++ Selbi/src/reducers/AddBankAccountReducer.js
@@ -0,0 +1,54 @@
+import Immutable from 'immutable';
+import { getActionType } from './ActionUtils';
+
+const BA_CLEAR = 'clear-bank-account-entry';
+const BA_SET_LEGAL_NAME = 'bank-account-set-legal-name';
+const BA_SET_DOB_DAY = 'bank-account-set-legal-name';
+
+class AddBankAccountData extends Immutable.Record({
+  legalName: undefined,
+  dobDay: undefined,
+  dobMonth: undefined,
+  dobYear: undefined,
+  addressLine1: undefined,
+  addressLine2: undefined,
+  addressCity: undefined,
+  addressPostalCode: undefined,
+  addressState: undefined,
+  routingNumber: undefined,
+  accountNumber: undefined,
+  ssn: undefined,
+}) {}
+
+export default function (previousState = new AddBankAccountData(), action) {
+  switch (getActionType(action)) {
+    case BA_CLEAR:
+      return new AddBankAccountData();
+    case BA_SET_DOB_DAY:
+      return previousState.merge({ dobDay: action.day });
+    case BA_SET_LEGAL_NAME:
+      return previousState.merge({ legalName: action.legalName });
+    default:
+      return previousState;
+  }
+}
+
+export function clearBankAccount() {
+  return {
+    type: BA_CLEAR,
+  };
+}
+
+export function setLegalName(legalName) {
+  return {
+    type: BA_SET_LEGAL_NAME,
+    legalName,
+  };
+}
+
+export function setDobDay(day) {
+  return {
+    type: BA_SET_DOB_DAY,
+    day,
+  };
+}
