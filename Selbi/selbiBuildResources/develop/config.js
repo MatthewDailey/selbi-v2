1e06e6d7259e34f9ea53df0edc36e813eeaf7de2
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-09-26 21:25:53 -0700

    add nearprinter stripe keys to Selbi, need to get Selbi keys from jordan

--- Selbi/selbiBuildResources/develop/config.js
+++ Selbi/selbiBuildResources/develop/config.js
@@ -6,4 +6,6 @@ export default {
     storageBucket: 'selbi-develop.appspot.com',
   },
   codePushKey: 'XYU_wAvoNeTzW_UuGogZ5kImnomjVybu8pY2Z',
+  // Note this key is to Nearprinter NOT Selbi.
+  stripePublicKey: 'pk_test_jgbSMlxI62OBXiWH1ENa9o5U',
 };
--- Selbi/selbiBuildResources/production/config.js
+++ Selbi/selbiBuildResources/production/config.js
@@ -6,4 +6,6 @@ export default {
     storageBucket: 'selbi-production.appspot.com',
   },
   codePushKey: 'y6B_DK6Pn0XGH0vNaqUegewScu0gVybu8pY2Z',
+  // Note this key is to Nearprinter NOT Selbi.
+  stripePublicKey: 'pk_live_w84euqaA53nDZ4fo5rnxIE4W',
 };
