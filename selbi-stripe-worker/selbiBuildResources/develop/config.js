804b5dfc15dec0d857ce1786f5df6d3e3380ba15
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-09-26 23:44:19 -0700

    refactor config to take advantage of process.env

--- selbi-stripe-worker/selbiBuildResources/develop/config.js
+++
@@ -1,5 +0,0 @@
-
-export default {
-  // This is the Nearprinter key.
-  stripePrivateKey: 'sk_test_RHjBCVkeOHrGiRQiZdViICm5',
-};
--- selbi-stripe-worker/selbiBuildResources/production/config.js
+++
@@ -1,5 +0,0 @@
-
-export default {
-  // This is the Nearprinter key.
-  stripePrivateKey: 'sk_live_DrssBizr5Ce5JIFJpydhACCA ',
-};
