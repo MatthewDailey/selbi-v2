6e4dd6cd0d3f5f957f2a5c8cf5b168c0540e185e
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-10-10 22:15:09 -0700

    use legacy api to fix gym build

--- Selbi/index.ios.js
+++ Selbi/index.ios.js
@@ -98,6 +98,7 @@ function fetchLocalListings() {
               center: [newLocation.lat, newLocation.lon],
             });
           }
+
         });
       })
       .then(() => startedListeningForLocalListings = true)
