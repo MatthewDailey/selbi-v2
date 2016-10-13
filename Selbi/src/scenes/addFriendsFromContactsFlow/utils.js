e732b2881830f1f1d18217c3a6d14c2173a4c904
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-10-12 19:42:13 -0700

    introduce react-native-contacts for loading contact data

--- Selbi/src/scenes/addFriendsFromContactsFlow/utils.js
+++ Selbi/src/scenes/addFriendsFromContactsFlow/utils.js
@@ -1,6 +1,31 @@
+import Contacts from 'react-native-contacts';
 
 export default undefined;
 
 export function normalizePhoneNumber(number) {
-  return `+1${number}`;
+  const cleanedNumber = number
+    .replace(/-/g, '')
+    .replace(/\(/g, '')
+    .replace(/\)/g, '')
+    .replace(/\s/g, '');
+  return `+1${cleanedNumber}`;
+}
+
+export function loadAllContactsPhoneNumber() {
+  return new Promise((resolve, reject) => {
+    Contacts.getAll((err, contacts) => {
+      if (err && err.type === 'permissionDenied') {
+        reject('Permission denied.');
+      } else if (err) {
+        reject(err);
+      } else {
+        const allPhoneNumbers = [];
+        contacts.forEach((contact) => {
+          contact.phoneNumbers.forEach((phone) =>
+            allPhoneNumbers.push(normalizePhoneNumber(phone.number)));
+        });
+        resolve(allPhoneNumbers);
+      }
+    });
+  });
 }
