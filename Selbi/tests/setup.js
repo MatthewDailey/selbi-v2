790ad6e6929bbeb2cfbf5b02ff063ed57d93d5c3
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-10-24 22:03:18 -0700

    enable transpilation prior to running npm test

--- Selbi/tests/setup.js
+++ Selbi/tests/setup.js
@@ -45,6 +45,8 @@ const modulesToCompile = [
   'react-native-scrollable-tab-view',
   'react-native-autogrow-textinput',
   'react-native-fcm',
+  'react-native-firebase-analytics',
+  'react-native-fbsdk',
 ].map((moduleName) => new RegExp(`/node_modules/${moduleName}`));
 const rcPath = path.join(__dirname, '..', '.babelrc');
 const source = fs.readFileSync(rcPath).toString();
