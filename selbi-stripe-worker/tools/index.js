4409295744c52b0e1f159d76ec5e97da58ddd8f1
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-10-28 10:16:10 -0700

    enable tools to run on various stages

--- selbi-stripe-worker/tools/index.js
+++ selbi-stripe-worker/tools/index.js
@@ -1,6 +1,21 @@
 require('babel-core/register');
 
-// Usage: node index.js <tool name> <tool args>
-const desiredApp = process.argv[2];
+// Usage: node index.js <stage> <tool name> <tool args>
+const desiredApp = process.argv[3];
+const targetStage = process.argv[2];
+
+if (targetStage !== 'individual'
+  && targetStage !== 'develop'
+  && targetStage !== 'staging'
+  && targetStage !== 'production') {
+  throw Error(`${targetStage} is not a valid stage.`);
+}
+
+if (targetStage === 'develop'
+  || targetStage === 'staging'
+  || targetStage === 'production') {
+  process.env.SELBI_CONFIG_FILE = 'non-existant-rc-filename';
+  process.env.SELBI_ENVIRONMENT = targetStage;
+}
 
 require(`./${desiredApp}`);
