40c310af63fd5db247554fc0d215d5078f060332
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-11-08 10:25:06 -0800

    refactor SellerProfileReducer to include profile data

--- Selbi/src/reducers/SellerProfileReducer.js
+++ Selbi/src/reducers/SellerProfileReducer.js
@@ -1,37 +1,40 @@
 import { getActionType } from './ActionUtils';
 
 const SP_SET_LISTINGS = 'seller-profile-set-listings';
-const SP_SET_UID = 'seller-profile-set-uid';
+const SP_SET_INFO = 'seller-profile-set-info';
 const SP_CLEAR = 'seller-profile-clear';
 
-
-
-export default function (priorState = { uid: undefined, listings: undefined }, action) {
+export default function (
+  priorState = { uid: undefined, sellerData: undefined, listings: undefined }, action) {
   switch (getActionType(action)) {
-    case SP_SET_UID:
+    case SP_SET_INFO:
       return {
         uid: action.uid,
         listings: undefined,
+        sellerData: action.sellerData,
       };
     case SP_SET_LISTINGS:
       return {
         uid: priorState.uid,
         listings: action.listings,
+        sellerData: priorState.sellerData,
       };
     case SP_CLEAR:
       return {
         uid: undefined,
         listings: undefined,
+        sellerData: undefined,
       };
     default:
       return priorState;
   }
 }
 
-export function setSellerProfileUid(uid) {
+export function setSellerProfileInfo(uid, sellerData) {
   return {
-    type: SP_SET_UID,
+    type: SP_SET_INFO,
     uid,
+    sellerData,
   };
 }
 
