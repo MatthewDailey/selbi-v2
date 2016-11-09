c814d708b1a5e4d4f39cefd89f5fa3090c489008
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-11-08 21:29:58 -0800

    use flatbutton for bulletins

--- Selbi/src/components/buttons/FlatButton.js
+++ Selbi/src/components/buttons/FlatButton.js
@@ -5,48 +5,48 @@ import { MKButton } from 'react-native-material-kit';
 import colors from '../../../colors';
 import styles from '../../../styles';
 
-export function FlatButton({
-  onPress,
-  children,
-  borderWidth = 1,
-  underlayColor = colors.primary,
-  backgroundColor = colors.white,
-}) {
-  return (
-    <TouchableHighlight
-      underlayColor={underlayColor}
-      onPress={onPress}
-      style={
-        [
-          styles.halfPadded,
-          {
-            borderRadius: 5,
-            borderWidth,
-            backgroundColor,
-          },
-        ]
-      }
-    >
-      <View
-        style={{
-          flex: 1,
-          alignItems: 'center',
-          borderRadius: 5,
-        }}
-      >
-        {children}
-      </View>
-    </TouchableHighlight>
-  );
-}
-
-FlatButton.propTypes = {
-  borderWidth: React.PropTypes.number,
-  onPress: React.PropTypes.func,
-  children: React.PropTypes.element,
-  backgroundColor: React.PropTypes.string,
-  underlayColor: React.PropTypes.string,
-};
+// export function FlatButton({
+//   onPress,
+//   children,
+//   borderWidth = 1,
+//   underlayColor = colors.primary,
+//   backgroundColor = colors.white,
+// }) {
+//   return (
+//     <TouchableHighlight
+//       underlayColor={underlayColor}
+//       onPress={onPress}
+//       style={
+//         [
+//           styles.halfPadded,
+//           {
+//             borderRadius: 5,
+//             borderWidth,
+//             backgroundColor,
+//           },
+//         ]
+//       }
+//     >
+//       <View
+//         style={{
+//           flex: 1,
+//           alignItems: 'center',
+//           borderRadius: 5,
+//         }}
+//       >
+//         {children}
+//       </View>
+//     </TouchableHighlight>
+//   );
+// }
+//
+// FlatButton.propTypes = {
+//   borderWidth: React.PropTypes.number,
+//   onPress: React.PropTypes.func,
+//   children: React.PropTypes.element,
+//   backgroundColor: React.PropTypes.string,
+//   underlayColor: React.PropTypes.string,
+// };
 
 export default function Button({
   onPress,
